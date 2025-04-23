6168faa9fb4b349fcfb99bd96fbd14898a24c90f
Chirag945 <118551445+Chirag945@users.noreply.github.com>
2025-04-24 00:45:40 +0530

    Update compilerFeedback.py

--- code-decom/compilerFeedback.py
+++ code-decom/compilerFeedback.py
@@ -7,22 +7,7 @@ def compile_or_run(code, language):
     # Determine the correct file extension
     if language.lower() in ["c++", "cpp"]:
         suffix = ".cpp"
-    elif language.lower() == "java":
-        suffix = ".java"
-    elif language.lower() == "python":
-        suffix = ".py"
-    else:
-        print("Unsupported language. Please choose C++, Java, or Python.")
-      
-        if language.lower() in ["c++", "cpp"]:
-            # Compile C++ and show all errors
-            output_executable = "a.out" if os.name != "nt" else "a.exe"
-            compile_command = ["g++", tmp_source, "-o", output_executable]
-            result = subprocess.run(compile_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            if result.returncode != 0:
-                return result.stderr
-            else:
-                return ("C++ Compilation succeeded!")
+    elif language.lower() == "jav
         elif language.lower() == "java":
             # Compile Java and show all errors
             compile_command = ["javac", tmp_source]
