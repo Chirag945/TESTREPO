ab66fd976e19a79c1cf813491c77bbcb29abef31
Chirag945 <118551445+Chirag945@users.noreply.github.com>
2025-04-23 23:39:58 +0530

    Add files via upload

---
+++ rough.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int             long long
+#define double          long double
+#define pb              push_back
+#define all(t)          (t).begin(), (t).end()
+#define rep(i,j)        for(int i = 0; i < (j); ++i)
+#define rrep(i,j)       for(int i = (j)-1; i >= 0; --i)
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<pair<int, int>> vpi;
+const char nl = '\n';
+const double eps = 1e-6;
+const int N = 1e5, mod = 1e9 + 7, inf = 1e18;
+#define yay cout<<"YES\n"
+#define nay cout<<"NO\n"
+#define vin(v) for(auto&x:v)cin>>x;
+#define print(v) for(auto x:v) cout<<x<<" "; cout<<"\n";
+
+void solve() {
+    bool ans1=true,ans2=true;
+    int n; cin>>n;
+    vi v(n); vin(v);
+    for(int i=0;i<n;i++){
+        if(((n-i-1)*2)<v[i]) ans1=false;
+    }
+    for(int i=n-1;i>-1;i--){
+        if((i*2)<v[i]) ans2=false;
+    }
+    if(ans1||ans2) yay;
+    else nay;
+    return;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+   cin >> T;//Comment this out in case number of test cases are not to be taken as input.
+    for(int I = 1; I <= T; I++) {
+         
+        solve(); 
+    }
+    return 0;
+}
